3cac25762be64114a2f6a9640a57fa1cd65ccd9a
duync2006 <reynold.cs.w3@gmail.com>
2022-11-23 23:13:20 +0700

    update MCP controller 2 #1

--- src/server/controllers/MCP.controller.js
+++ src/server/controllers/MCP.controller.js
@@ -2,7 +2,6 @@ const MCP = require('../models/MCP.model');
 
 //-----------GET ALL-------------//
 exports.getAllMCPs = async (req, res) => {
-  console.log(req)
   try {
     const MCPs = await MCP.findAll();
     console.log("Get all MCPs success.")
@@ -28,10 +27,18 @@ exports.getMCP = async (req, res) => {
   }
   try {
     const MCPByID = await MCP.findByPk(req.params.id);
-    res.json ({
-      status: 'success',
-      data: MCPByID
-    })  
+    if (!MCPByID){
+      res.json({
+        status: '404',
+        message: 'MCP ID not found'
+      })
+    }
+    else {
+      res.json ({
+        status: 'success',
+        data: MCPByID
+      })
+    }  
   } catch (err) {
     next(err)
   }
@@ -41,25 +48,27 @@ exports.getMCP = async (req, res) => {
 exports.createMCP = async (req, res) => {
     if (!req.body) {
       console.err(err)
-      res.status(400).json({
-        status: 'error',
+      res.json({
+        status: '400',
         message: "Content can not be empty!"
       });
     }
+    else {
     try {
-      console.log(req.body)
-      const newMCP = await MCP.create({
-        long: req.body.long,
-        lat: req.body.lat,
-        isFull: req.body.isFull,
-        type: req.body.type,
-      });
-      res.json({
-        status: 'success',
-        data: newMCP
-      })
-    } catch (err) {
-      next(err)
+        console.log(req.body)
+        const newMCP = await MCP.create({
+          long: req.body.long,
+          lat: req.body.lat,
+          isFull: req.body.isFull,
+          type: req.body.type,
+        });
+        res.json({
+          status: 'success',
+          data: newMCP
+        })
+      }catch (err) {
+        next(err)
+      }
     }
 }
 
@@ -99,10 +108,18 @@ exports.removeMCP = async (req, res) =>{
       id: id
     }
   });
+  if (removing == 0)
+  {
+    res.json({
+      status: '204',
+    })
+  }
+  else {
   res.json({
-    status: 'success',
-    data: removing
-  })  
+      status: 'success',
+      data: removing
+    })
+  }  
   } catch (err) {
     next(err)
   }
